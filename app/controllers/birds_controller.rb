f2ccff3f0c605a237186d9f9aa2765fa3a2abbfa
Sophie Gu <sophieqgu@gmail.com>
2021-01-12 04:05:42 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,5 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
-  end
+    render json: { message: 'Hashes of data will get converted to JSON' }
 end
